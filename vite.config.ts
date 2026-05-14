b59abc5aab88cd51f58062c909f604d8e18e725f
Urangani <www.uranganidc@gmail.com>
2026-05-14 16:43:50 +0200

    fixed cors e

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    watch: { usePolling: true },
+  },
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
